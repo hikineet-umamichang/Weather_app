4ce6eeaf4ec5945b29790f2be514286bb661b500
hikineet-umamichang <1308yuta@gmail.com>
2023-03-19 11:10:24 +0000

    Modifications for publishing on PythonAnywhere

--- test.py
+++ test.py
@@ -2,6 +2,10 @@
 
 import requests
 import os
+from dotenv import load_dotenv
+
+project_home="/home/umamichang/Weather_app"
+load_dotenv(os.path.join(project_home, '.env'))
 
 API_KEY = os.environ.get("WEATHER_API_KEY")
 CITY_NAME = "London"
@@ -10,7 +14,7 @@ BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 params = {"q": CITY_NAME, "appid": API_KEY, "units": "metric"}
 
 response = requests.get(BASE_URL, params=params)
-
+print(API_KEY)
 if response.status_code == 200:
     data = response.json()
     main = data["main"]
